398765328a6095e8380acd270180469d3cc40b13
Denis Zhokhov <d3z41k@gmail.com>
2019-08-14 10:17:54 +0200

    Remove compress from router

--- server/server.go
+++ server/server.go
@@ -22,7 +22,6 @@ func NewRouter() http.Handler {
 	router.Use(middleware.RealIP)
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
-	router.Use(middleware.DefaultCompress)
 	router.Use(middleware.Timeout(60 * time.Second))
 
 	//Set up root handler
