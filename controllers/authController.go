778c2b0933b5dfee94fbbd18e89119dd035f5ff7
Denis Zhokhov <d3z41k@gmail.com>
2019-08-16 16:42:20 +0200

    Add auth controller

---
+++ controllers/authController.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/d3z41k/rest-boilerplate/models"
+	u "github.com/d3z41k/rest-boilerplate/utils"
+)
+
+// CreateAccount - create new account from request data
+var CreateAccount = func(w http.ResponseWriter, r *http.Request) {
+	account := &models.Account{}
+	err := json.NewDecoder(r.Body).Decode(account)
+	if err != nil {
+		u.Respond(w, u.Message(false, "Invalid request"))
+		return
+	}
+
+	resp := account.Create()
+	u.Respond(w, resp)
+}
+
+// Authenticate account from request data
+var Authenticate = func(w http.ResponseWriter, r *http.Request) {
+	account := &models.Account{}
+	err := json.NewDecoder(r.Body).Decode(account)
+	if err != nil {
+		u.Respond(w, u.Message(false, "Invalid request"))
+		return
+	}
+
+	resp := models.Login(account.Email, account.Password)
+	u.Respond(w, resp)
+}
