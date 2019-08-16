877ac717febad94cd51155ab8cd73f9c8199835f
Denis Zhokhov <d3z41k@gmail.com>
2019-08-16 17:08:31 +0200

    Add contacts controller

---
+++ controllers/contactsController.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/d3z41k/rest-boilerplate/models"
+	u "github.com/d3z41k/rest-boilerplate/utils"
+)
+
+// CreateContact - create user contact
+var CreateContact = func(w http.ResponseWriter, r *http.Request) {
+	userID := r.Context().Value("user").(uint)
+	contact := &models.Contact{}
+
+	err := json.NewDecoder(r.Body).Decode(contact)
+	if err != nil {
+		u.Respond(w, u.Message(false, "Error while decoding request body"))
+		return
+	}
+
+	contact.UserID = userID
+	resp := contact.Create()
+	u.Respond(w, resp)
+}
+
+// GetContactsFor - get user contacts
+var GetContactsFor = func(w http.ResponseWriter, r *http.Request) {
+	userID := r.Context().Value("user").(uint)
+	data := models.GetContacts(userID)
+	resp := u.Message(true, "success")
+	resp["data"] = data
+	u.Respond(w, resp)
+}
